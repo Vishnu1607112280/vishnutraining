517c0009e3cd900d4eba8dfbd6e98fb780359aa9
vishnuvardhan pokala <105355757+Vishnu1607112280@users.noreply.github.com>
2022-06-30 09:26:21 +0530

    Add files via upload

---
+++ discoveryserver/src/test/java/com/zensar/springbootdemodiscoveryserver/DiscoveryserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zensar.springbootdemodiscoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
